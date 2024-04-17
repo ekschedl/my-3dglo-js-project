34294196e80aa0880f469b12c9a90a8d71b82b7d
Ekaterina Schedlberger <ek.schedl@gmail.com>
2024-04-17 11:36:21 +0200

    +webpack dev server

--- src/modules/two.js
+++ src/modules/two.js
@@ -1,6 +1,7 @@
 const twoModule = () => {
   const two = "two.js works";
   console.log(two);
+  console.log("test test");
 };
 
 export default twoModule;
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  entry: "./index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 9000,
+    open: true,
+    client: {
+      overlay: true, // Включить вывод ошибок в браузер
+    },
+  },
+};
