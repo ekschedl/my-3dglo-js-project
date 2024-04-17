e8520e3121d96dff0b26049027e7dc3082205bbf
Ekaterina Schedlberger <ek.schedl@gmail.com>
2024-04-17 10:31:19 +0200

    start

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import oneModule from "./modules/one";
-import twoModule from "./modules/two";
+import timer from "./modules/timer";
+//import twoModule from "./modules/two";
 
-oneModule();
-twoModule();
+timer("23 april 2024");
+//twoModule();
