f0e8e3117614cbfed9e8a2f268c14fba18d8a03d
Ekaterina Schedlberger <ek.schedl@gmail.com>
2024-04-18 12:18:30 +0200

    correction requestAnimationFrame

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,20 +1,46 @@
 const modal = () => {
   const buttons = document.querySelectorAll(".popup-btn");
-
   const modal = document.querySelector(".popup");
   const closeBtn = modal.querySelector(".popup-close");
-  console.log(closeBtn);
 
   buttons.forEach((btn) => {
     btn.addEventListener("click", () => {
       modal.style.display = "block";
+      fadeIn(modal);
     });
   });
 
   closeBtn.addEventListener("click", () => {
-    modal.style.display = "none";
+    fadeOut(modal);
   });
 
   document.querySelector(".popup").classList.add("active");
 };
+
+const fadeIn = (element) => {
+  let opacity = 0;
+  const fadeInAnimation = () => {
+    opacity += 0.05; // Увеличиваем прозрачность на 0.05 за каждый кадр
+    element.style.opacity = opacity;
+    if (opacity < 1) {
+      requestAnimationFrame(fadeInAnimation);
+    }
+  };
+  requestAnimationFrame(fadeInAnimation);
+};
+
+const fadeOut = (element) => {
+  let opacity = 1;
+  const fadeOutAnimation = () => {
+    opacity -= 0.05; // Уменьшаем прозрачность на 0.05 за каждый кадр
+    element.style.opacity = opacity;
+    if (opacity > 0) {
+      requestAnimationFrame(fadeOutAnimation);
+    } else {
+      element.style.display = "none";
+    }
+  };
+  requestAnimationFrame(fadeOutAnimation);
+};
+
 export default modal;
