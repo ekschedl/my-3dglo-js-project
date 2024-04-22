72c262475655625d8aa8b35db34ac5f2b9654176
Ekaterina Schedlberger <ek.schedl@gmail.com>
2024-04-23 00:44:58 +0200

    may be solution

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,30 +1,63 @@
+//Ксения. я оставила это пока для себя, я удалю потом
+// const menu = () => {
+//   const menuBtn = document.querySelector(".menu");
+//   const menu = document.querySelector("menu");
+//   const closeBtn = menu.querySelector(".close-btn");
+//   const menuItems = menu.querySelectorAll("ul>li>a");
+
+//   const handleMenu = () => {
+//     //   if (!menu.style.transform) {
+//     //     menu.style.transform = "translateX(0)";
+//     //   } else menu.style.transform = "";
+//     // или так
+//     menu.classList.toggle("active-menu");
+//   };
+
+//   menuBtn.addEventListener("click", handleMenu);
+
+//   closeBtn.addEventListener("click", handleMenu);
+
+//   // можно так
+//   // for (let i = 0; i < menuItems.length; i++) {
+//   //   console.log(menuItems[i]);
+//   //   menuItems[i].addEventListener("click", handleMenu); // }
+
+//   // а можно так
+//   menuItems.forEach((menuItem) =>
+//     menuItem.addEventListener("click", handleMenu)
+//   );
+// };
+
+// export default menu;
+
 const menu = () => {
   const menuBtn = document.querySelector(".menu");
   const menu = document.querySelector("menu");
   const closeBtn = menu.querySelector(".close-btn");
-  const menuItems = menu.querySelectorAll("ul>li>a");
+  const menuItems = menu.querySelectorAll("ul > li > a");
 
   const handleMenu = () => {
-    //   if (!menu.style.transform) {
-    //     menu.style.transform = "translateX(0)";
-    //   } else menu.style.transform = "";
-    // или так
     menu.classList.toggle("active-menu");
   };
 
-  menuBtn.addEventListener("click", handleMenu);
+  const handleClick = (event) => {
+    const target = event.target;
 
-  closeBtn.addEventListener("click", handleMenu);
+    if (target.closest(".menu") === menuBtn) {
+      handleMenu();
+    } else if (target === closeBtn) {
+      handleMenu();
+    } else if (
+      target.closest("menu") &&
+      Array.from(menuItems).includes(target)
+    ) {
+      handleMenu();
+    } else if (!target.closest("menu")) {
+      menu.classList.remove("active-menu");
+    }
+  };
 
-  // можно так
-  // for (let i = 0; i < menuItems.length; i++) {
-  //   console.log(menuItems[i]);
-  //   menuItems[i].addEventListener("click", handleMenu); // }
-
-  // а можно так
-  menuItems.forEach((menuItem) =>
-    menuItem.addEventListener("click", handleMenu)
-  );
+  document.addEventListener("click", handleClick);
 };
 
 export default menu;
